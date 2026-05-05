9a439f2a9b73653fc155cad0c3ffdae979dd16dc
RonaldCrack10 <rashidgaming6@gmail.com>
2026-05-05 09:05:27 +0200

    init corrected for demo to be started

--- GestureRecognition/modules/__init__.py
+++ GestureRecognition/modules/__init__.py
@@ -2,3 +2,7 @@ from .trailmarker import TrailMarker
 from .hiddenmarkov import HMMModule
 from .handdetector import HandDetector
 from .preprocessor import Preprocessor
+from GestureRecognition.modules.handdetector import HandDetector
+from GestureRecognition.modules.trailmarker import TrailMarker
+from GestureRecognition.modules.preprocessor import Preprocessor
+from GestureRecognition.modules.hiddenmarkov import HMMModule
