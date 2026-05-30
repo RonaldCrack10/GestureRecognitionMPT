5b7e1a096a5884a2016e93b8eaa718c4c2836c3a
RonaldCrack10 <rashidgaming6@gmail.com>
2026-05-30 15:46:20 +0200

    training the model and testing whether it identifies gestures

---
+++ train.py
@@ -0,0 +1,10 @@
+import pickle
+from GestureRecognition.hmmclassifier import HMMClassifier
+
+with open("data/dataset.pickle", "rb") as f:
+    ds = pickle.load(f)
+
+clf = HMMClassifier(n_components=4, n_iter=100, test_size=0.2)
+clf.fit(ds["X"], ds["lengths"], ds["labels"])
+clf.evaluate(ds["X"], ds["lengths"], ds["labels"])
+clf.save("data/hmm_model.pickle")
